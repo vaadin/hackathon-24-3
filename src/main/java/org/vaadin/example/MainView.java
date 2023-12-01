bd26f26300d686b6d506ded14e1f6bcaa0e1df9b
Sascha Ißbrücker <sissbruecker@vaadin.com>
2023-12-01 13:55:24 +0100

    Add back message input

--- src/main/java/org/vaadin/example/MainView.java
+++ src/main/java/org/vaadin/example/MainView.java
@@ -64,6 +64,9 @@ public class MainView extends VerticalLayout {
         upload.setAutoUpload(false);
         layout.add(upload);
 
+        LocalizedMessageInput messageInput = new LocalizedMessageInput();
+        layout.add(messageInput);
+
         return layout;
     }
 
