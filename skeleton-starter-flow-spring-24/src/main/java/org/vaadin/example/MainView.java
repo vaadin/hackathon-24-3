5457940b6469be602e4fa2dab2638a4c07b9ba40
Olli Tietäväinen <ollit@vaadin.com>
2023-12-01 15:08:44 +0200

    tested new default i18n provider

---
+++ skeleton-starter-flow-spring-24/src/main/java/org/vaadin/example/MainView.java
@@ -0,0 +1,61 @@
+package org.vaadin.example;
+
+import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.VaadinSession;
+
+import java.util.Locale;
+
+/**
+ * A sample Vaadin view class.
+ * <p>
+ * To implement a Vaadin view just extend any Vaadin component and use @Route
+ * annotation to announce it in a URL as a Spring managed bean.
+ * <p>
+ * A new instance of this class is created for every new user and every browser
+ * tab/window.
+ * <p>
+ * The main view contains a text field for getting the user name and a button
+ * that shows a greeting message in a notification.
+ */
+@Route
+public class MainView extends VerticalLayout {
+
+    /**
+     * Construct a new Vaadin view.
+     * <p>
+     * Build the initial UI state for the user accessing the application.
+     */
+    public MainView() {
+
+        // Use TextField for standard text input
+        Button localeButton = new Button("Change locale",
+                event -> VaadinSession.getCurrent().setLocale(Locale.ENGLISH));
+        add(localeButton);
+        Button button = new Button("Say hello", e -> {
+            add(new Paragraph(getTranslation("hello")));
+        });
+
+        button.addClassName("main-view-button-1");
+
+        // Theme variants give you predefined extra styles for components.
+        // Example: Primary button has a more prominent look.
+        button.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+
+        // You can specify keyboard shortcuts for buttons.
+        // Example: Pressing enter in this view clicks the Button.
+        button.addClickShortcut(Key.ENTER);
+
+        // Use custom CSS classes to apply styling. This is defined in
+        // styles.css.
+        addClassName("centered-content");
+
+        add(button);
+    }
+
+}
